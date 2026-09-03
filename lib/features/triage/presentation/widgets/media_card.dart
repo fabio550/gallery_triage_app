80ebc5d40b0458566267837f513b6b5cc272f750
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-09-03 17:24:50 -0300

    Add MediaCard widget with customizable color

---
+++ lib/features/triage/presentation/widgets/media_card.dart
@@ -0,0 +1,25 @@
+class MediaCard extends StatelessWidget {
+  final Color color;
+  
+  MediaCard({
+    required this.color,
+    super.key
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 400,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [BoxShadow(
+          color: Colors.black.withValues(),
+          blurRadius: 8,
+          offset: const Offset(0, 4),
+        )],
+      ),
+    );
+  }
+}
