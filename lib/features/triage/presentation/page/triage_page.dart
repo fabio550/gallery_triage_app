d46cf743b8bd8d123b6d07bda4cd39cadd2c118d
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-09-03 17:22:52 -0300

    Enhance TriagePage with item count and layout changes
    Updated TriagePage layout to include item count and improved structure.

--- lib/features/triage/presentation/page/triage_page.dart
+++ lib/features/triage/presentation/page/triage_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_triage_app/core/domain/models/category_summary.dart';
-
 class TriagePage extends StatelessWidget {
   final CategorySummary category;
   
@@ -11,12 +10,51 @@ class TriagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    final text = Theme.of(context).textTheme;
+    final List<Color> items = [
+      Colors.green,
+      Colors.red,
+      Colors.orange,
+      Colors.amber,
+      Colors.blueGrey,
+      Colors.indigo,
+      Colors.green,
+      Colors.red,
+      Colors.orange,
+      Colors.amber,
+      Colors.blueGrey,
+      Colors.indigo,
+    ];
+
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text(category.label),
+        title: Column(
+          children: [
+            Text(category.label),
+            Text(
+              'Item X de ${category.totalItems}',
+              style: text.bodySmall,
+            ),
+          ]
+        ),
       ),
-      body: Center(
-        child: Text('data')
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 40),
+              child: ProgressBar(
+              showLegend: true,
+              totalItems: category.totalItems,
+              classifiedItems: category.classifiedItems,
+              keptItems: category.keptItems,
+            ),
+          ),
+          Expanded(
+            child: TriageCarousel(items: items),
+          )
+        ]
       ),
     );
   }
