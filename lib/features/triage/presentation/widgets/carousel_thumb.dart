d0a13a928d56710e3867fb46bd3d105e45d09de7
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-09-03 17:23:58 -0300

    Add CarouselThumb widget for carousel navigation

---
+++ lib/features/triage/presentation/widgets/carousel_thumb.dart
@@ -0,0 +1,45 @@
+class CarouselThumb extends StatelessWidget {
+  final int index;
+  final bool isSelected;
+  final Color color;
+  final VoidCallback onTap;
+  
+  const CarouselThumb({
+    required this.index,
+    required this.isSelected,
+    required this.color,
+    required this.onTap,
+    super.key,
+  });
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeOut,
+        width: 90,
+        height: 90,
+        margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+          border: isSelected
+              ? Border.all(color: Colors.white, width: 3)
+              : null,
+          boxShadow: isSelected
+            ? [
+                BoxShadow(
+                  color: Colors.black.withValues(),
+                  blurRadius: 8,
+                  offset: const Offset(0, 4),
+                )
+              ]
+            : [],
+        ),
+      ),
+    );
+  }
+}
